d64d620294f6943c8b9a3629eafb3b14d826854a
Drake Arconis <drake@alchemyviewer.org>
2014-12-14 01:51:50 -0500

    Welp this builds.

--- build-cmd.sh
+++ build-cmd.sh
@@ -9,16 +9,22 @@ stage="$(pwd)"
 cd "$(dirname "$0")"
 TOP="$(pwd)"
 
-PROJECT="gtk-etc"
-ATK_VERSION="1.30.0"
+PROJECT="gtk-atk-pango-glib"
+GLIB_VERSION="2.36.4"
+GLIB_SOURCE_DIR="glib-$GLIB_VERSION"
+ATK_VERSION="2.8.0"
 ATK_SOURCE_DIR="atk-$ATK_VERSION"
-PIXMAN_VERSION="0.17.14"
+PIXMAN_VERSION="0.30.2"
 PIXMAN_SOURCE_DIR="pixman-$PIXMAN_VERSION"
-CAIRO_VERSION="1.8.10"
+CAIRO_VERSION="1.12.14"
 CAIRO_SOURCE_DIR="cairo-$CAIRO_VERSION"
-PANGO_VERSION="1.28.4"
+HARFBUZZ_VERSION="0.9.20"
+HARFBUZZ_SOURCE_DIR="harfbuzz-$HARFBUZZ_VERSION"
+PANGO_VERSION="1.34.1"
 PANGO_SOURCE_DIR="pango-$PANGO_VERSION"
-GTK_VERSION="2.20.1"
+GDK_PIXBUF_VERSION="2.28.2"
+GDK_PIXBUF_SOURCE_DIR="gdk-pixbuf-$GDK_PIXBUF_VERSION"
+GTK_VERSION="2.24.23"
 GTK_SOURCE_DIR="gtk+-$GTK_VERSION"
 
 if [ -z "$AUTOBUILD" ] ; then 
@@ -36,14 +42,6 @@ eval "$AUTOBUILD install"
 set -x
 
 
-setup_packages()
-{
-   # sadly pkgconfig doesn't actually understand prefix=${PREBUILD_DIR}
-   find "$1" -type f  -name '*.pc' \
-       -exec perl -pi -e "\$bar='$stage/packages';s/\\$\{PREBUILD_DIR\}/\$bar/" {} \;
-   rm -f "$1"/../*.la
-}
-
 build_linux()
 {
          LIB_LINK_DIR="$stage/packages/lib/release"
@@ -52,46 +50,69 @@ build_linux()
          export LD_RUN_PATH="$LIB_LINK_DIR:$LD_RUN_PATH"
          export LD_LIBRARY_PATH="$LIB_LINK_DIR:$LD_LIBRARY_PATH"
          export LDFLAGS="-m$1 -L$LIB_LINK_DIR"
-         export CFLAGS="-O3 -m$1 -msse2 -mfpmath=sse -pipe -ftree-vectorize"
-         export CXXFLAGS=$CFLAGS
+         export CFLAGS="-O3 -m$1 -I$stage/packages/include -I$stage/packages/include/libpng16"
+         export CXXFLAGS="$CFLAGS -std=c++11"
          export PKG_CONFIG_PATH="$stage/packages/lib/release/pkgconfig"
-
-         setup_packages $PKG_CONFIG_PATH
 
          # we don't want to rebuild gtk each time one of these is updated
          # so kill the shared versions 
          rm -f "$stage"/packages/lib/release/libxml2.so*
          rm -f "$stage"/packages/lib/release/libz.so*
 
+         fix_pkgconfig_prefix "$stage/packages"
+         rm -f $stage/packages/lib/release/*.la
+
+         pushd "$TOP/$GLIB_SOURCE_DIR"
+         ./configure --prefix="\${AUTOBUILD_PACKAGES_DIR}"    \
+                     --libdir="\${prefix}/lib/release" \
+                     --includedir="\${prefix}/include" \
+                     --disable-selinux    \
+                     --with-libiconv=no   \
+                     --with-threads=posix \
+                     --with-pcre=internal
+ 
+         make $MAKE_OPTIONS
+         make install DESTDIR="$stage"
+         make install DESTDIR="$stage/packages"
+ 
+         cp -f $stage/lib/release/glib-2.0/include/glibconfig.h $stage/include/glib-2.0/glibconfig.h
+ 
+         make distclean
+         fix_pkgconfig_prefix "$stage/packages"
+         rm -f $stage/packages/lib/release/*.la
+         popd
+ 
          pushd "$TOP/$ATK_SOURCE_DIR"
-         ./configure --prefix="\${PREBUILD_DIR}"    \
+         ./configure --prefix="\${AUTOBUILD_PACKAGES_DIR}"    \
                      --libdir="\${prefix}/lib/release" \
                      --includedir="\${prefix}/include" \
                      --disable-rpath
-
+ 
          make $MAKE_OPTIONS
          make install DESTDIR="$stage"
          make install DESTDIR="$stage/packages"
          make distclean
-         setup_packages $PKG_CONFIG_PATH
+         fix_pkgconfig_prefix "$stage/packages"
+         rm -f $stage/packages/lib/release/*.la
          popd
-
+ 
          pushd "$TOP/$PIXMAN_SOURCE_DIR"
-         ./configure --prefix="\${PREBUILD_DIR}"    \
+         ./configure --prefix="\${AUTOBUILD_PACKAGES_DIR}"    \
                      --libdir="\${prefix}/lib/release" \
                      --includedir="\${prefix}/include" \
                      --disable-rpath \
                      --disable-gtk
-
+ 
          make $MAKE_OPTIONS
          make install DESTDIR="$stage"
          make install DESTDIR="$stage/packages"
          make distclean
-         setup_packages $PKG_CONFIG_PATH
+         fix_pkgconfig_prefix "$stage/packages"
+         rm -f $stage/packages/lib/release/*.la
          popd
-
+ 
          pushd "$TOP/$CAIRO_SOURCE_DIR"
-         ./configure --prefix="\${PREBUILD_DIR}"    \
+         ./configure --prefix="\${AUTOBUILD_PACKAGES_DIR}"    \
                      --libdir="\${prefix}/lib/release" \
                      --includedir="\${prefix}/include" \
                      --enable-png=yes  \
@@ -103,16 +124,35 @@ build_linux()
                      --enable-xml=no   \
                      --disable-rpath \
                      --enable-pthread=yes
+ 
+         make $MAKE_OPTIONS
+         make install DESTDIR="$stage"
+         make install DESTDIR="$stage/packages"
+         make distclean
+         fix_pkgconfig_prefix "$stage/packages"
+         rm -f $stage/packages/lib/release/*.la
+         popd
+
+         pushd "$TOP/$HARFBUZZ_SOURCE_DIR"
+         ./configure --prefix="\${AUTOBUILD_PACKAGES_DIR}" \
+                     --libdir="\${prefix}/lib/release" \
+                     --includedir="\${prefix}/include" \
+                     --enable-gtk-doc-html=no
 
          make $MAKE_OPTIONS
          make install DESTDIR="$stage"
          make install DESTDIR="$stage/packages"
          make distclean
-         setup_packages $PKG_CONFIG_PATH
+
+         cp $TOP/extras/*.pc $stage/lib/release/pkgconfig/
+         cp $TOP/extras/*.pc $stage/packages/lib/release/pkgconfig/
+
+         fix_pkgconfig_prefix "$stage/packages"
+         rm -f $stage/packages/lib/release/*.la
          popd
 
          pushd "$TOP/$PANGO_SOURCE_DIR"
-         ./configure --prefix="\${PREBUILD_DIR}"    \
+         ./configure --prefix="\${AUTOBUILD_PACKAGES_DIR}"    \
                      --libdir="\${prefix}/lib/release" \
                      --includedir="\${prefix}/include" \
                      --disable-rpath \
@@ -122,21 +162,43 @@ build_linux()
          make install DESTDIR="$stage"
          make install DESTDIR="$stage/packages"
          make distclean
-         setup_packages $PKG_CONFIG_PATH
+         fix_pkgconfig_prefix "$stage/packages"
+         rm -f $stage/packages/lib/release/*.la
          popd
 
-         pushd "$TOP/$GTK_SOURCE_DIR"
-         ./configure --prefix="\${PREBUILD_DIR}"    \
+         pushd "$TOP/$GDK_PIXBUF_SOURCE_DIR"
+         ./configure --prefix="\${AUTOBUILD_PACKAGES_DIR}"    \
                      --libdir="\${prefix}/lib/release" \
                      --includedir="\${prefix}/include" \
-                     --disable-rpath \
+                     --enable-gtk-doc-html=no \
+                     --with-libpng   \
+                     --without-libjpeg  \
+                     --without-libtiff  \
+                     --with-x11 \
+                     --disable-rpath
+
+         make $MAKE_OPTIONS
+         make install DESTDIR="$stage"
+         make install DESTDIR="$stage/packages"
+         make distclean
+         fix_pkgconfig_prefix "$stage/packages"
+         rm -f $stage/packages/lib/release/*.la
+         popd
+
+         export GDK_PIXBUF_MODULE_FILE="$stage/packages/lib/release/gdk-pixbuf-2.0/2.10.0/loaders.cache"
+         export GDK_PIXBUF_MODULEDIR="$stage/packages/lib/release/gdk-pixbuf-2.0/2.10.0/loaders"
+         PATH="$stage/packages/bin:$PATH" gdk-pixbuf-query-loaders > $stage/packages/lib/release/gdk-pixbuf-2.0/2.10.0/loaders.cache
+
+         pushd "$TOP/$GTK_SOURCE_DIR"
+         PATH="$stage/packages/bin:$PATH" \
+         ./configure --prefix="\${AUTOBUILD_PACKAGES_DIR}"    \
+                     --libdir="\${prefix}/lib/release" \
+                     --includedir="\${prefix}/include" \
                      --enable-explicit-deps=no \
                      --disable-papi     \
                      --disable-cups     \
                      --disable-glibtest \
                      --enable-gtk-doc-html=no \
-                     --without-libjpeg  \
-                     --without-libtiff  \
                      --with-x
 
         make $MAKE_OPTIONS
@@ -145,11 +207,6 @@ build_linux()
         popd
 
         cp -f "$stage"/lib/release/gtk-2.0/include/gdkconfig.h "$stage"/include/gtk-2.0/gdkconfig.h
-
-        # ceterum censeo gtk is a pain to build (see above),
-        # a major reason for crashes and malfunctions (see any viewers bugtracker),
-        # a quite heavy dependency for just opening windows and handling the clipboard,
-        # and can't do anything Qt wouldn't do at least equally well.
 }
 
 case "$AUTOBUILD_PLATFORM" in
@@ -168,8 +225,12 @@ esac
 
 mkdir -p "$stage/LICENSES"
 
-LICENSE_FILE="$stage/LICENSES/gtk-etc.txt"
+LICENSE_FILE="$stage/LICENSES/gtk-atk-pango-glib.txt"
 
+echo "----------------------------" >> $LICENSE_FILE
+echo "$GLIB_SOURCE_DIR License:" >> $LICENSE_FILE
+echo "----------------------------" >> $LICENSE_FILE
+cat "$TOP/$GLIB_SOURCE_DIR/COPYING" >> $LICENSE_FILE
 
 echo "----------------------------" >> $LICENSE_FILE
 echo "$ATK_SOURCE_DIR License:" >> $LICENSE_FILE
@@ -187,20 +248,24 @@ echo "----------------------------" >> $LICENSE_FILE
 cat "$TOP/$CAIRO_SOURCE_DIR/COPYING" >> $LICENSE_FILE
 
 echo "----------------------------" >> $LICENSE_FILE
+echo "$HARFBUZZ_SOURCE_DIR License:" >> $LICENSE_FILE
+echo "----------------------------" >> $LICENSE_FILE
+cat "$TOP/$HARFBUZZ_SOURCE_DIR/COPYING" >> $LICENSE_FILE
+
+echo "----------------------------" >> $LICENSE_FILE
 echo "$PANGO_SOURCE_DIR License:" >> $LICENSE_FILE
 echo "----------------------------" >> $LICENSE_FILE
 cat "$TOP/$PANGO_SOURCE_DIR/COPYING" >> $LICENSE_FILE
 
 echo "----------------------------" >> $LICENSE_FILE
+echo "$GDK_PIXBUF_SOURCE_DIR License:" >> $LICENSE_FILE
+echo "----------------------------" >> $LICENSE_FILE
+cat "$TOP/$GDK_PIXBUF_SOURCE_DIR/COPYING" >> $LICENSE_FILE
+
+echo "----------------------------" >> $LICENSE_FILE
 echo "$GTK_SOURCE_DIR License:" >> $LICENSE_FILE
 echo "----------------------------" >> $LICENSE_FILE
 cat "$TOP/$GTK_SOURCE_DIR/COPYING" >> $LICENSE_FILE
-
-README_DIR="$stage/autobuild-bits"
-README_FILE="$README_DIR/README-Version-3p-$PROJECT"
-mkdir -p $README_DIR
-cat $TOP/.hg/hgrc|grep default |sed  -e "s/default = ssh:\/\/hg@/https:\/\//" > $README_FILE
-echo "Commit $(hg id -i)" >> $README_FILE
 
 pass
 
--- configure-cmd.sh
+++ configure-cmd.sh
@@ -1,59 +1,88 @@
 #!/bin/bash
 
-ATK_VERSION="1.30.0"
+GLIB_VERSION="2.36.4"
+GLIB_SOURCE_DIR="glib-$GLIB_VERSION"
+ATK_VERSION="2.8.0"
 ATK_SOURCE_DIR="atk-$ATK_VERSION"
-PIXMAN_VERSION="0.17.14"
+PIXMAN_VERSION="0.30.2"
 PIXMAN_SOURCE_DIR="pixman-$PIXMAN_VERSION"
-CAIRO_VERSION="1.8.10"
+CAIRO_VERSION="1.12.14"
 CAIRO_SOURCE_DIR="cairo-$CAIRO_VERSION"
-PANGO_VERSION="1.28.4"
+PANGO_VERSION="1.34.1"
+HARFBUZZ_VERSION="0.9.20"
+HARFBUZZ_SOURCE_DIR="harfbuzz-$HARFBUZZ_VERSION"
 PANGO_SOURCE_DIR="pango-$PANGO_VERSION"
-GTK_VERSION="2.20.1"
+GDK_PIXBUF_VERSION="2.28.2"
+GDK_PIXBUF_SOURCE_DIR="gdk-pixbuf-$GDK_PIXBUF_VERSION"
+GTK_VERSION="2.24.23"
 GTK_SOURCE_DIR="gtk+-$GTK_VERSION"
 
 TOP="$(dirname "$0")"
 pushd $TOP
 echo "checking for all source dirs"
 
+if [ -d "$GLIB_SOURCE_DIR" ]; then
+   echo "$GLIB_SOURCE_DIR is present"
+else
+   echo "$GLIB_SOURCE_DIR not present, downloading"
+   wget http://ftp.acc.umu.se/pub/GNOME/sources/glib/2.36/glib-$GLIB_VERSION.tar.xz
+   tar -xvf glib-$GLIB_VERSION.tar.xz
+fi
 
 if [ -d "$ATK_SOURCE_DIR" ]; then
    echo "$ATK_SOURCE_DIR is present"
 else
    echo "$ATK_SOURCE_DIR not present, downloading"
-   wget http://ftp.gnome.org/pub/GNOME/sources/atk/1.30/atk-1.30.0.tar.gz
-   tar -xzf atk-1.30.0.tar.gz
+   wget http://ftp.gnome.org/pub/GNOME/sources/atk/2.8/atk-$ATK_VERSION.tar.xz
+   tar -xvf atk-$ATK_VERSION.tar.xz
 fi
 
 if [ -d "$PIXMAN_SOURCE_DIR" ]; then
    echo "$PIXMAN_SOURCE_DIR is present"
 else
    echo "$PIXMAN_SOURCE_DIR not present, downloading"
-   wget http://cairographics.org/snapshots/pixman-0.17.14.tar.gz
-   tar -xzf pixman-0.17.14.tar.gz
+   wget http://cairographics.org/releases/pixman-$PIXMAN_VERSION.tar.gz
+   tar -xvf pixman-$PIXMAN_VERSION.tar.gz
 fi
 
 if [ -d "$CAIRO_SOURCE_DIR" ]; then
    echo "$CAIRO_SOURCE_DIR is present"
 else
    echo "$CAIRO_SOURCE_DIR not present, downloading"
-   wget http://cairographics.org/releases/cairo-1.8.10.tar.gz
-   tar -xzf cairo-1.8.10.tar.gz
+   wget http://cairographics.org/releases/cairo-$CAIRO_VERSION.tar.xz
+   tar -xvf cairo-$CAIRO_VERSION.tar.xz
+fi
+
+if [ -d "$HARFBUZZ_SOURCE_DIR" ]; then
+   echo "$HARFBUZZ_SOURCE_DIR is present"
+else
+   echo "$HARFBUZZ_SOURCE_DIR not present, downloading"
+   wget http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-$HARFBUZZ_VERSION.tar.bz2
+   tar -xvf harfbuzz-$HARFBUZZ_VERSION.tar.bz2
 fi
 
 if [ -d "$PANGO_SOURCE_DIR" ]; then
    echo "$PANGO_SOURCE_DIR is present"
 else
    echo "$PANGO_SOURCE_DIR not present, downloading"
-   wget http://ftp.gnome.org/pub/gnome/sources/pango/1.28/pango-1.28.4.tar.gz
-   tar -xzf pango-1.28.4.tar.gz
+   wget http://ftp.gnome.org/pub/gnome/sources/pango/1.34/pango-$PANGO_VERSION.tar.xz
+   tar -xvf pango-$PANGO_VERSION.tar.xz
+fi
+
+if [ -d "$GDK_PIXBUF_SOURCE_DIR" ]; then
+   echo "$GDK_PIXBUF_SOURCE_DIR is present"
+else
+   echo "$GDK_PIXBUF_SOURCE_DIR not present, downloading"
+   wget http://ftp.gnome.org/pub/gnome/sources/gdk-pixbuf/2.28/gdk-pixbuf-$GDK_PIXBUF_VERSION.tar.xz
+   tar -xvf gdk-pixbuf-$GDK_PIXBUF_VERSION.tar.xz
 fi
 
 if [ -d "$GTK_SOURCE_DIR" ]; then
    echo "$GTK_SOURCE_DIR is present"
 else
    echo "$GTK_SOURCE_DIR not present, downloading"
-   wget http://ftp.gnome.org/pub/gnome/sources/gtk+/2.20/gtk+-2.20.1.tar.gz
-   tar -xzf gtk+-2.20.1.tar.gz
+   wget http://ftp.gnome.org/pub/gnome/sources/gtk+/2.24/gtk+-$GTK_VERSION.tar.xz
+   tar -xvf gtk+-$GTK_VERSION.tar.xz
 fi
 
 # if [ -d "$" ]; then
